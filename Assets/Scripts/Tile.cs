45a938fd53ceba88562369ebc4e98fd61adf6767
Spencer <splant.10@gmail.com>
2018-11-10 19:35:27 -0500

    Cleaned up code

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,8 +9,8 @@ public class Tile : MonoBehaviour
 	
 	public int _value;
 	
-	public Vector2 homePosition; // where it will slide back to if not over a valid slot
-	public Vector2 targetPosition; // if over a valid slot, will slide to this point.
+	public Vector3 homePosition; // where it will slide back to if not over a valid slot
+	public Vector3 targetPosition; // if over a valid slot, will slide to this point.
 
 	// Update is called once per frame
 	void Update ()
@@ -38,22 +38,32 @@ public class Tile : MonoBehaviour
 
 	void slide()
 	{
-		if (overTarget())
-		{
-			slideToPoint(targetPosition);
-		}
-		else
-		{
-			slideToPoint(homePosition);
-		}
+		Vector3 position = overTarget() ? targetPosition : homePosition;
+		slideToPoint(homePosition);
 	}
 
 	Boolean overTarget()
 	{
-		return false;
+		// Bit shift the index of the layer (15) to get a bit mask
+		int layerMask = 1 << 15;
+
+		// This will cast rays only against colliders in layer 15.
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+		{
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+			Debug.Log("Did Hit");
+		}
+		else
+		{
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+			Debug.Log("Did not Hit");
+		}
+		
 		// I'm thinking ray-cast back until you hit either a "target" or an invisible "non-target" field.
 		// non-target field is a large plane that rests right below any valid targets, so other targets on the (future)
 		// wheel won't be able to be targeted, because they should be behind this plane.
+		return false;
 	}
 
 	void slideToPoint(Vector3 point)
