69672fabba79379c84ea15ab62d03028c95af2a5
Spencer <splant.10@gmail.com>
2018-11-11 20:21:50 -0500

    stuff

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-	private int _distanceFromCamera = 13;
+	private int _distanceFromCamera = 14;
 	
 	public int value;
 	public string tileOperator = null;
