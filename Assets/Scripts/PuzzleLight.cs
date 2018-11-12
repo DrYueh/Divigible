a16a063eb0d3c65c51baaff68dc29873d468a169
asampley <sampley@ualberta.ca>
2018-11-12 10:01:52 -0700

    Added light to indicate completion of puzzle

---
+++ Assets/Scripts/PuzzleLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class PuzzleLight : MonoBehaviour {
+    public GameManager gameManager;
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameManager.puzzleSolved()) {
+            GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
+        } else {
+            GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+        }
+	}
+}
