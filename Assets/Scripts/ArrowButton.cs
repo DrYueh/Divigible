764554753c90ce57a682ddb27db885c02c44a0c1
Spencer <splant.10@gmail.com>
2018-11-11 21:56:17 -0500

    button arrows

---
+++ Assets/Scripts/ArrowButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ArrowButton : MonoBehaviour
+{
+	public Boolean up;
+
+	private void OnMouseUpAsButton()
+	{
+		//throw new System.NotImplementedException();
+		if (up)
+		{
+			GameObject.Find("GameManager").GetComponent<GameManager>().changeTestByDraggingUp(true);
+			print("move up");
+		}
+		else
+		{
+			GameObject.Find("GameManager").GetComponent<GameManager>().changeTestByDraggingUp(false);
+			print("move down");
+		}
+	}
+}
