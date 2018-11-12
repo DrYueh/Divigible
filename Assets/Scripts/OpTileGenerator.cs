d84ff81c0c1778250fef1c3b05c8a5ced84337fe
asampley <sampley@ualberta.ca>
2018-11-11 17:25:10 -0700

    Added operator generation

---
+++ Assets/Scripts/OpTileGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpTileGenerator : MonoBehaviour { 
+    public GameObject addPrefab;
+    public GameObject subPrefab;
+    public GameObject multPrefab;
+    public GameObject divPrefab;
+    public GameObject eqPrefab; 
+    
+    public Tile generateTile(Vector3 location, string op) {
+        GameObject prefab = null;
+        switch (op) {
+            case "+": prefab = addPrefab; break;
+            case "-": prefab = subPrefab; break;
+            case "*": prefab = multPrefab; break;
+            case "/": prefab = divPrefab; break;
+            case "=": prefab = eqPrefab; break;
+            default: return null;
+        }
+        GameObject obj = Instantiate(prefab);
+        obj.transform.position = location;
+        obj.GetComponent<Tile>().layerToTarget = 20;
+        
+        return obj.GetComponent<Tile>();
+    }
+}
