2436a2d820a1ae5ee3e958f8b7f94208c41eb339
asampley <sampley@ualberta.ca>
2018-11-12 11:42:23 -0700

    Added panel to fade out when the game ends

---
+++ Assets/Scripts/ImageFade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFade : MonoBehaviour {
+    public GameManager gameManager;
+    public float timeToFade;
+    public float fadePeriod;
+
+	// Update is called once per frame
+	void Update () {
+        if (timeToFade < Time.time && !gameManager.puzzleSolved()) {
+            Image image = GetComponent<Image>();
+            Color color = image.color;
+            image.color = new Color(color.r, color.g, color.b, Mathf.Clamp((Time.time - timeToFade) / fadePeriod, 0, 1));
+        }
+	}
+}
